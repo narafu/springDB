e83807085820b2fbdcbfd549a871c0a477206f34
phs <phs0918@imedisync.com>
2022-08-24 20:43:07 +0900

    ConnectionPoll & dataSource

---
+++ src/test/java/hello/jdbc/connection/ConnectionTest.java
@@ -0,0 +1,64 @@
+package hello.jdbc.connection;
+
+import com.zaxxer.hikari.HikariDataSource;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import static hello.jdbc.connection.ConnectionConst.*;
+
+@Slf4j
+public class ConnectionTest {
+
+    @Test
+    void driverManager() throws SQLException {
+
+        /** DriverManager는 파라미터를 매번 필요로 한다. */
+
+        Connection con1 = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        Connection con2 = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        log.info("connection={}, class={}", con1, con1.getClass());
+        log.info("connection={}, class={}", con2, con2.getClass());
+
+    }
+
+    @Test
+    void dataSourceDriverManager() throws SQLException {
+
+        /** DataSource는 설정과 사용을 분리한다.
+         * 한번 설정해주면, 파라미터 없이 getConnection()으로 connection을 얻을 수 있다.
+         * * * */
+
+        // DriverManagerDataSource - 항상 새로운 커넥션을 획득
+        DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
+        useDataSource(dataSource);
+    }
+
+    private void useDataSource(DataSource dataSource) throws SQLException {
+        Connection con1 = dataSource.getConnection();
+        Connection con2 = dataSource.getConnection();
+        log.info("connection={}, class={}", con1, con1.getClass());
+        log.info("connection={}, class={}", con2, con2.getClass());
+    }
+
+    @Test
+    void dataSourceConnectionPool() throws Exception {
+
+        // 커넥션 풀링
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(URL);
+        dataSource.setUsername(USERNAME);
+        dataSource.setPassword(PASSWORD);
+        dataSource.setMaximumPoolSize(10);
+        dataSource.setPoolName("MyPool");
+
+        useDataSource(dataSource);
+        Thread.sleep(1000);
+    }
+
+}
--- src/test/java/hello/jdbc/repository/MemberRepositoryV1Test.java
+++ src/test/java/hello/jdbc/repository/MemberRepositoryV1Test.java
@@ -1,22 +1,38 @@
 package hello.jdbc.repository;
 
+import com.zaxxer.hikari.HikariDataSource;
 import hello.jdbc.domain.Member;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.sql.SQLException;
 import java.util.NoSuchElementException;
 
+import static hello.jdbc.connection.ConnectionConst.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @Slf4j
-class MemberRepositoryVOTest {
+class MemberRepositoryV1Test {
 
-    MemberRepositoryVO repository = new MemberRepositoryVO();
+    MemberRepositoryV1 repository;
+
+    @BeforeEach
+    void beforeEach() {
+        // 기본 DriverManager = 항상 새로운 커넥션 획득
+//        DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
+
+        // 커넥션 풀링
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(URL);
+        dataSource.setUsername(USERNAME);
+        dataSource.setPoolName(PASSWORD);
+
+        repository = new MemberRepositoryV1(dataSource);
+    }
 
     @Test
-    void crud() throws SQLException {
+    void crud() throws Exception {
 
         // save
         Member member = new Member("memberV100", 10000);
@@ -38,6 +54,8 @@ class MemberRepositoryVOTest {
         repository.delete(member.getMemberId());
         assertThatThrownBy(() -> repository.findById(member.getMemberId()))
                 .isInstanceOf(NoSuchElementException.class);
+
+        Thread.sleep(1000);
     }
 
 }
