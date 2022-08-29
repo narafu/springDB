ba640286ac3b19b9b8980f4d5bde6d01660adcf9
narafu <narafu@kakao.com>
2022-08-29 19:43:26 +0900

    Transaction 3-12까지

--- src/main/java/hello/jdbc/service/MemberServiceV1.java
+++ src/main/java/hello/jdbc/service/MemberServiceV1.java
@@ -12,6 +12,7 @@ public class MemberServiceV1 {
     private final MemberRepositoryV1 memberRepository;
 
     public void accountTransfer(String fromId, String toId, int money) throws SQLException {
+
         Member fromMember = memberRepository.findById(fromId);
         Member toMember = memberRepository.findById(toId);
 
