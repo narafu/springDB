cba7b36951e8f63b8a86ec46881aee565306f2fe
narafu <narafu@kakao.com>
2022-09-06 20:32:59 +0900

    section 6. 스프링과 문제 해결 (~정리)

--- src/test/java/hello/jdbc/service/MemberServiceV4Test.java
+++ src/test/java/hello/jdbc/service/MemberServiceV4Test.java
@@ -3,6 +3,7 @@ package hello.jdbc.service;
 import hello.jdbc.domain.Member;
 import hello.jdbc.repository.MemberRepository;
 import hello.jdbc.repository.MemberRepositoryV4_1;
+import hello.jdbc.repository.MemberRepositoryV5;
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
@@ -43,7 +44,9 @@ class MemberServiceV4Test {
     @BeforeEach
     void before() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
-        memberRepository = new MemberRepositoryV4_1(dataSource);
+//        memberRepository = new MemberRepositoryV4_1(dataSource);
+//        memberRepository = new MemberRepositoryV4_2(dataSource);
+        memberRepository = new MemberRepositoryV5(dataSource);
     }
 
     @TestConfiguration
@@ -99,7 +102,6 @@ class MemberServiceV4Test {
         Member findMemberB = memberRepository.findById(memberB.getMemberId());
         assertThat(findMemberA.getMoney()).isEqualTo(8000);
         assertThat(findMemberB.getMoney()).isEqualTo(12000);
-
     }
 
     @Test
@@ -121,7 +123,6 @@ class MemberServiceV4Test {
         Member findMemberB = memberRepository.findById(memberEx.getMemberId());
         assertThat(findMemberA.getMoney()).isEqualTo(10000);
         assertThat(findMemberB.getMoney()).isEqualTo(10000);
-
     }
 
 }
